bfaf4fcd62bb4c3b5b79c670b072cf6c2f91e5e0
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2025-08-29 00:33:59 -0300

    Arquivos do módulo 01 - Fundamentos Node.js

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import http from 'node:http'
+import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
+import { extratQueryParams } from './utils/extract-query-params.js'
+
+const server = http.createServer(async (req, res) => {
+    const { method, url } = req
+
+    await json(req, res)
+
+    const route = routes.find(route => {
+        return route.method === method && route.path.test(url)
+    })
+
+    if (route) {
+        const routeParams = req.url.match(route.path)
+
+        const { query, ...params } = routeParams.groups
+
+        req.params = params
+
+        req.query = query ? extratQueryParams(query) : {}
+
+        return route.handler(req, res)
+    }
+
+    return res.writeHead(404).end()
+})
+
+// localhost:3333
+server.listen(3333)
